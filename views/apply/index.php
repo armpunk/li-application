0fbdd900b08a1109b06b45c5fdff6d6e620750e1
Farid Yusof <faridyusof727@gmail.com>
2017-08-17 15:57:38 +0800

    sss

--- views/apply/index.php
+++ views/apply/index.php
@@ -1,56 +1,68 @@
 <?php
 /* @var $this yii\web\View */
+
 use yii\bootstrap\ActiveForm;
 use dosamigos\selectize\SelectizeDropDownList;
 use yii\helpers\ArrayHelper;
 use app\models\Department;
 use kartik\widgets\FileInput;
-
 ?>
 <h3>LI Application Form</h3>
 
 <?php
-    if (Yii::$app->session->hasFlash('success')) {
-        echo yii\bootstrap\Alert::widget([
-            'options' => [
-                'class' => 'alert-success'
-            ],
-            'body' => Yii::$app->session->getFlash('success')
-        ]);
-    }
+if (Yii::$app->session->hasFlash('success')) {
+    echo yii\bootstrap\Alert::widget([
+        'options' => [
+            'class' => 'alert-success'
+        ],
+        'body' => Yii::$app->session->getFlash('success')
+    ]);
+}
 ?>
 
 <?php $form = ActiveForm::begin() ?>
 
 <?= $form->field($studentModel, 'full_name') ?>
-<?= $form->field($studentModel, 'ic_no')->widget(\yii\widgets\MaskedInput::className(), [
+<?=
+$form->field($studentModel, 'ic_no')->widget(\yii\widgets\MaskedInput::className(), [
     'mask' => '999999999999',
-]); ?>
+]);
+?>
 <?= $form->field($studentModel, 'email') ?>
 
 <div class="form-group">
     <label>Choose Preferred Department</label>
-    <?php echo SelectizeDropDownList::widget([
-    'name' => 'departments[]',
-    'items' => ArrayHelper::map(Department::find()->all(), 'id', 'name'),
-    'clientOptions' => [
-        'maxItems' => 3
-    ],
-]); ?>
+    <?php
+    echo SelectizeDropDownList::widget([
+        'name' => 'departments[]',
+        'items' => ArrayHelper::map(Department::find()->all(), 'id', 'name'),
+        'clientOptions' => [
+            'maxItems' => 3
+        ],
+    ]);
+    ?>
 </div>
 
 <div class="form-group">
     <label>Document Attachments</label>
-  <?= FileInput::widget([
-    'model' => $uploadModel,
-    'attribute' => 'attachmentFiles[]',
-    'options' => ['multiple' => true]
-]) ?>  
+    <?=
+    FileInput::widget([
+        'model' => $uploadModel,
+        'attribute' => 'attachmentFiles[]',
+        'options' => [
+            'multiple' => true,
+            'accept' => '.docx, .pdf, .xlsx, .pptx'
+        ],
+        'pluginOptions' => [
+            'maxFileCount' => 3
+        ]
+    ])
+    ?>  
 </div>
 
 
 
-<?= \yii\helpers\Html::submitButton('Submit Application', 
-        ['class' => 'btn btn-primary']) ?>
+<?= \yii\helpers\Html::submitButton('Submit Application', ['class' => 'btn btn-primary'])
+?>
 
 <?php ActiveForm::end() ?>
